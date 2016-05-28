66748c58475d775b37c46044a49a60d617bef226
Christopher Rackauckas <Me@ChrisRackauckas.com>
2016-05-28 08:58:09 -0700

    Initial Commit

--- src/GrowableArrays.jl
+++ src/GrowableArrays.jl
@@ -1,5 +1,76 @@
 module GrowableArrays
 
-# package code goes here
+immutable GrowableArray{T,A,N} <: AbstractArray{T,N}
+    data::Vector{A}
+end
+#=
+function GrowableArray(elem::Number)
+  data = Vector{typeof(elem)}(0)
+  push!(data,elem)
+  GrowableArray{typeof(elem),typeof(elem),1}(data)
+end
+=#
+
+function GrowableArray(elem)
+  data = Vector{typeof(elem)}(0)
+  push!(data,elem)
+  if typeof(elem) <: AbstractArray
+    GrowableArray{eltype(elem),typeof(elem),ndims(elem)+1}(data)
+  else
+    GrowableArray{typeof(elem),typeof(elem),1}(data)
+  end
+end
+Base.sizehint!(G::GrowableArray,i::Int) = sizehint!(G.data,i)
+Base.size(G::GrowableArray) = (length(G.data), size(G.data[1])...)
+Base.getindex(G::GrowableArray, i::Int) = G.data[i] # expand a linear index out
+Base.getindex(G::GrowableArray, i::Int, I::Int...) = G.data[i][I...]
+function Base.setindex!(G::GrowableArray, elem,i::Int) ##TODO: Add type checking on elem
+  G.data[ind2sub(size(G), i)...] = elem
+end
+function Base.setindex!(G::GrowableArray, elem,i::Int,I::Int...)
+  G.data[i][I...] = elem
+end
+function Base.push!(G::GrowableArray,elem)
+  push!(G.data,elem)
+end
+
+immutable StackedArray{T,N,A} <: AbstractArray{T,N}
+    data::A
+    dims::NTuple{N,Int}
+end
+StackedArray(vec::AbstractVector) = StackedArray(vec, (length(vec), size(vec[1])...)) # TODO: ensure all elements are the same size
+StackedArray{A<:AbstractVector, N}(vec::A, dims::NTuple{N}) = StackedArray{eltype(eltype(A)),N,A}(vec, dims)
+Base.size(S::StackedArray) = S.dims
+Base.getindex(S::StackedArray, i::Int) = S.data[ind2sub(size(S), i)...] # expand a linear index out
+Base.getindex(S::StackedArray, i::Int, I::Int...) = S.data[i][I...]
+
+import Base.getindex, Base.setindex!
+const .. = Val{:...}
+
+setindex!{T}(A::AbstractArray{T,1}, x, ::Type{Val{:...}}, n::Int) = A[n] = x
+setindex!{T}(A::AbstractArray{T,2}, x, ::Type{Val{:...}}, n::Int) = A[ :, n] = x
+setindex!{T}(A::AbstractArray{T,3}, x, ::Type{Val{:...}}, n::Int) = A[ :, :, n] =x
+setindex!{T}(A::AbstractArray{T,4}, x, ::Type{Val{:...}}, n::Int) = A[ :, :, :, n] =x
+setindex!{T}(A::AbstractArray{T,5}, x, ::Type{Val{:...}}, n::Int) = A[ :, :, :, :, n] =x
+
+getindex{T}(A::AbstractArray{T,1}, ::Type{Val{:...}}, n::Int) = A[n]
+getindex{T}(A::AbstractArray{T,2}, ::Type{Val{:...}}, n::Int) = A[ :, n]
+getindex{T}(A::AbstractArray{T,3}, ::Type{Val{:...}}, n::Int) = A[ :, :, n]
+getindex{T}(A::AbstractArray{T,4}, ::Type{Val{:...}}, n::Int) = A[ :, :, :, n]
+getindex{T}(A::AbstractArray{T,5}, ::Type{Val{:...}}, n::Int) = A[ :, :, :, :, n]
+
+setindex!{T}(A::AbstractArray{T,1}, x, n::Int, ::Type{Val{:...}}) = A[n] = x
+setindex!{T}(A::AbstractArray{T,2}, x, n::Int, ::Type{Val{:...}}) = A[n, :] = x
+setindex!{T}(A::AbstractArray{T,3}, x, n::Int, ::Type{Val{:...}}) = A[n, :, :] =x
+setindex!{T}(A::AbstractArray{T,4}, x, n::Int, ::Type{Val{:...}}) = A[n, :, :, :] =x
+setindex!{T}(A::AbstractArray{T,5}, x, n::Int, ::Type{Val{:...}}) = A[n, :, :, :, :] =x
+
+getindex{T}(A::AbstractArray{T,1}, n::Int, ::Type{Val{:...}}) = A[n]
+getindex{T}(A::AbstractArray{T,2}, n::Int, ::Type{Val{:...}}) = A[n, :]
+getindex{T}(A::AbstractArray{T,3}, n::Int, ::Type{Val{:...}}) = A[n, :, :]
+getindex{T}(A::AbstractArray{T,4}, n::Int, ::Type{Val{:...}}) = A[n, :, :, :]
+getindex{T}(A::AbstractArray{T,5}, n::Int, ::Type{Val{:...}}) = A[n, :, :, :, :]
+
+export StackedArray, GrowableArray, setindex!, getindex, ..
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,176 @@
 using GrowableArrays
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+function test1()
+  u =    [1 2 3 4
+          1 3 3 4
+          1 5 6 3
+          5 2 3 1]
+
+  uFull = u
+  for i = 1:10000
+    uFull = hcat(uFull,u)
+  end
+  uFull
+end
+
+function test2()
+  u =    [1 2 3 4
+          1 3 3 4
+          1 5 6 3
+          5 2 3 1]
+
+  uFull = u
+
+  for i = 1:10000
+    uFull = vcat(uFull,u)
+  end
+  uFull
+end
+
+function test3()
+  u =    [1 2 3 4
+          1 3 3 4
+          1 5 6 3
+          5 2 3 1]
+
+  uFull = Vector{Int}(0)
+  sizehint!(uFull,10000*16)
+  append!(uFull,vec(u))
+
+  for i = 1:10000
+    append!(uFull,vec(u))
+  end
+  reshape(uFull,4,4,10001)
+  uFull
+end
+
+function test4()
+  u =    [1 2 3 4
+          1 3 3 4
+          1 5 6 3
+          5 2 3 1]
+
+  uFull = Vector{Array{Int}}(0)
+  push!(uFull,u)
+
+  for i = 1:10000
+    push!(uFull,u)
+  end
+  uFull
+end
+
+function test5()
+  u =    [1 2 3 4
+          1 3 3 4
+          1 5 6 3
+          5 2 3 1]
+
+  uFull = Vector{Array{Int,2}}(0)
+  push!(uFull,u)
+
+  for i = 1:10000
+    push!(uFull,u)
+  end
+  uFull
+end
+
+function test6()
+  u =    [1 2 3 4
+          1 3 3 4
+          1 5 6 3
+          5 2 3 1]
+
+  uFull = Vector{typeof(u)}(0)
+  push!(uFull,u)
+
+  for i = 1:10000
+    push!(uFull,u)
+  end
+  uFull
+end
+
+function test7()
+  u =    [1 2 3 4
+          1 3 3 4
+          1 5 6 3
+          5 2 3 1]
+
+  uFull = GrowableArray(u)
+  for i = 1:10000
+    push!(uFull,u)
+  end
+  uFull
+end
+
+function test8()
+  u =    [1 2 3 4
+          1 3 3 4
+          1 5 6 3
+          5 2 3 1]
+
+  uFull = GrowableArray(u)
+  sizehint!(uFull,10000)
+  for i = 1:10000
+    push!(uFull,u)
+  end
+  uFull
+end
+
+println("Run Benchmarks")
+println("Pre-Compile")
+#Compile Test Functions
+test1()
+test2()
+test3()
+test4()
+test5()
+test6()
+test7()
+test8()
+
+println("Running Benchmarks")
+numRuns = 10
+t1 = @elapsed for i=1:numRuns test1() end
+t2 = @elapsed for i=1:numRuns test2() end
+t3 = @elapsed for i=1:numRuns test3() end
+t4 = @elapsed for i=1:numRuns test4() end
+t5 = @elapsed for i=1:numRuns test5() end
+t6 = @elapsed for i=1:numRuns test6() end
+t7 = @elapsed for i=1:numRuns test7() end
+t8 = @elapsed for i=1:numRuns test8() end
+
+println("Benchmark results: $t1 $t2 $t3 $t4 $t5 $t6 $t7 $t8")
+println("Note the overhead of building the type before the loop for small runs")
+A = [1 2; 3 4]
+B = [1 2; 4 3]
+G = GrowableArray(A)
+push!(G,A)
+push!(G,A)
+push!(G,A)
+@test begin
+  Garr = copy(G) #Should change it to a regular array
+  typeof(Garr)<:AbstractArray{Int,3}
+end
+
+G2 = GrowableArray(1)
+push!(G2,3)
+G[4] = B
+G[4,..] = B
+@test G[4] == B
+@test reshape(G[4,..],2,2) == B #This acts as a standard array
+u =    [1 2 3 4
+        1 3 3 4
+        1 5 6 3
+        5 2 3 1]
+
+uFull = Vector{Array{Int,2}}(0)
+push!(uFull,u)
+
+for i = 1:100
+  push!(uFull,u)
+end
+S = StackedArray(uFull)
+K = S[1,..] + S[3,..]
+Sarr = copy(S)
+@test typeof(Sarr)<:AbstractArray{Int,3}
